4fa441334b16a164b9fac8239391d151e1fcdf0b
Christian Dalager <christian@dalager.com>
2014-05-02 23:24:56 +0200

    putting conwayERuleEngine into separate file

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,34 +2,7 @@
 
 
 
-var ConwayRuleEngine = function(){
-};
-ConwayRuleEngine.prototype.shouldCellChange = function(i,j,cell,neighbours){
-	// 4. Any dead cell with exactly three live neighbours 
-	//    becomes a live cell, as if by reproduction.
-	if(neighbours===3 && cell.alive!==true){
-		//$log.info('LIFE!')
-		return {'row':i,'cell':j,alive:true};
-	}
-	// rules
-	// 1. Any live cell with fewer than two live neighbours dies
-	//    as if caused by under-population.
-	if(cell.alive===true && neighbours < 2){
-		return {'row':i,'cell':j,alive:false};
-	}
 
-	//2. Any live cell with two or three live neighbours 
-	//   lives on to the next generation.
-	if(cell.alive===true && neighbours===2||neighbours===3){
-		return null;
-	}
-	// 3. Any live cell with more than three live neighbours dies, 
-	//    as if by overcrowding.
-	if(cell.alive===true && neighbours > 3){
-		return {'row':i,'cell':j,alive:false};
-	}
-};
-	
 angular.module('angularconwayApp')
   .controller('MainCtrl', function ($scope,$log,$interval) {
 	$scope.seedpct = 64;
